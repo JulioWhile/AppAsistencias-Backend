48869b49342a94544aeb5155344cfb42a74321fa
Julio Conteras <julio.contreras0429@gmail.com>
2020-12-10 19:56:19 -0700

    Se implementa jest para realizar pruebas y se crea carpeta de pruebas

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+describe('Pruebas en app.js', () => {
+	test('debe de iniciar el servidor', () => {
+		expect(true).toBe(true);
+	});
+});
